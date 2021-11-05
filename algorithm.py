7d0f001e05f83dba403640cb3fb1feb8134ba809
hello world <42101911+mraa022@users.noreply.github.com>
2021-11-05 14:05:05 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,27 +1,32 @@
 COLORS = ['red','green','blue','yellow','black','white']
 def find_deg_most_5(adjacency_matrix,removed_vs):
-    degree = lambda vertex: sum(adjacency_matrix[vertex])
+    # if G is a simple planar graph there is always a v such that deg(v)<=5.
+    # this function finds such a v
+    degree = lambda vertex: sum(adjacency_matrix[vertex]) # the sum of entries in a row of an adjacency matrix is that vertix's degree
     for vertex in range(len(adjacency_matrix[0])):
         if degree(vertex)<=5 and vertex not in removed_vs:
             return vertex
     
 def color_most_six(adjacency_matrix,v_list):
+    # if |V|<= 6 then it can be colored with at most 6 colors
+    # since you can just give each vertex a different color
     coloring = {}
     num_verticies = len(adjacency_matrix[0])    
     for vertex in range(num_verticies):
         if(vertex not in v_list):
-            coloring[vertex] = COLORS[vertex%6]
+            coloring[vertex] = COLORS[vertex%6] # %6 is there because the vertex's number might be >5 (list out of range)
     return coloring
     
 def set_difference(set1,set2):
     return [x for x in set1 if x not in set2]
     
 def neighbor_colors(v,adjacency_matrix,coloring):
-    neighbors = []
+    # a function that funds the colors of a vertex's neighbors
+    neighbors_c = []
     for i in range(len(adjacency_matrix[0])):
         if adjacency_matrix[v][i] ==1 and i in coloring:
-            neighbors.append(coloring[i])
-    
+            neighbors_c.append(coloring[i])
+    return neighbors_c
 def color_graph(adjacency_matrix):
     coloring = {}
     num_verticies = len(adjacency_matrix[0])
@@ -30,11 +35,15 @@ def color_graph(adjacency_matrix):
     
     else:
         removed_v = {}
+        
+        # first color 6 Vs
         while num_verticies>6:
            v = find_deg_most_5(adjacency_matrix,removed_v)
            removed_v[v] = 1
            num_verticies -=1
         coloring = color_most_six(adjacency_matrix,removed_v)
+        
+        # then color the rest
         for vertex in removed_v.keys():
             v_neighbor_colors = neighbor_colors(vertex,adjacency_matrix,coloring)
             v_color = set_difference(COLORS,v_neighbor_colors)[0]
